1e09debd9293d647c3d5a0737bd97a1d344b19a8
Hou-Tianqi <2702200569@qq.com>
2026-05-01 10:57:15 +0800

    背景颜色

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,7 @@ import MsgBoard from './components/MsgBoard.vue';
 <MsgBoard></MsgBoard>
 
 <footer>
-<p>如果你有Github账号，请给这个项目一个Star，如果star超过3个，我就加留言功能</p>
-<a href="https://github.com/Hou-Tianqi/hou-tianqi" id="re" target="_blank">点此访问本网页的GitHub仓库（可能需要加速器）</a>
-<p>如果你没有GitHub账号的话，就赶快去注册一个</p>
-<p>😭骗你的点不到3个star也得加</p>
+  太好啦我们已经完成了留言板的开发，但是DeepSeek写的
 </footer>
 
 <!--🤫如果你是看到了源码的人（很少可能会看到）你接下来一年里都会好运爆棚-->
