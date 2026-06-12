e7902838fee172039b7d857c8d3fc35db5b78bde
Hou-Tianqi <2702200569@qq.com>
2026-06-12 20:28:26 +0800

    没有音乐了，而是一个神奇表情包

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,26 @@
 <script setup>
 import Hello from './components/Hello.vue';
 import ShowMsgBoard from './components/ShowMsgBoard.vue';
-import Music from './components/Music.vue';
 
+import DidYouKnow from './components/DidYouKnow.vue';
 </script>
 
 <template>
 
 <Hello/>
+<DidYouKnow/>
 <ShowMsgBoard/>
-<Music/>
+
+<div style="text-align: center;">
+<h2>这是我刚学Blender做出来的一个红温表情包动画，感觉挺有意思的，就放这了</h2><br>
+<img src="../public/我已红温.gif" alt="啊哦，加载失败">
+</div>
+
 <br>
 <footer>
   <p style="font-size: 10px;">之所以上面的字体那么大是因为要照顾移动端而我不会写自动缩放之类的</p>
   太好啦我们已经完成了留言板的开发，但是DeepSeek写的
 </footer>
-<ul>
-  <h1 style="font-size: 100px;">以防你不知道：</h1>
-  <li><table>
-    <th>阿默斯特学院有核弹掩体吗？</th>
-    <tr>
-      <td>你所期望的答案：</td>
-      <td>没有</td>
-    </tr>
-    <tr>
-      <td>实际上：</td>
-      <td>有</td>
-    </tr>
-  </table></li>
-  <li><table>
-    <th>维基百科中文网能在中国直接访问吗？</th>
-    <tr>
-      <td>你所期望的答案：</td>
-      <td>能</td>
-    </tr>
-    <tr>
-      <td>实际上：</td>
-      <td>不能</td>
-    </tr>
-    </table></li>
-</ul>
 </template>
 
 <style scoped>
@@ -47,9 +28,5 @@ footer{
   font-size: 7px;
   text-align: center;
   color: grey;
-}
-td,
-th{
-  font-size: 30px;
 }
 </style>
---
+++ src/components/DidYouKnow.vue
@@ -0,0 +1,34 @@
+<template>
+<ul>
+  <h1 style="font-size: 100px;">以防你不知道：</h1>
+  <li><table>
+    <th>阿默斯特学院有核弹掩体吗？</th>
+    <tr>
+      <td>你所期望的答案：</td>
+      <td>没有</td>
+    </tr>
+    <tr>
+      <td>实际上：</td>
+      <td>有</td>
+    </tr>
+  </table></li>
+  <li><table>
+    <th>维基百科中文网能在中国直接访问吗？</th>
+    <tr>
+      <td>你所期望的答案：</td>
+      <td>能</td>
+    </tr>
+    <tr>
+      <td>实际上：</td>
+      <td>不能</td>
+    </tr>
+    </table></li>
+</ul>
+</template>
+
+<style scoped>
+td,
+th{
+  font-size: 30px;
+}
+</style>
